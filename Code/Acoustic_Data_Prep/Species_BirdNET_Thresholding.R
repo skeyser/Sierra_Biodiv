313e37f40c842b5947f081125cf4d4fdedea03cc
Spencer Ryan Keyser <srk252@cornell.edu>
2025-04-29 17:05:02 -0500

    Modifications to different selection criteria.

--- Code/Acoustic_Data_Prep/Species_BirdNET_Thresholding.R
+++ Code/Acoustic_Data_Prep/Species_BirdNET_Thresholding.R
@@ -91,11 +91,13 @@ for(s in 1:length(species.list_2021)){
   cutoff85.c=(log(.85/(1-.85))-tmp.mod.c$coefficients[1])/tmp.mod.c$coefficients[2]
   cutoff90.c=(log(.90/(1-.90))-tmp.mod.c$coefficients[1])/tmp.mod.c$coefficients[2]
   cutoff95.c=(log(.95/(1-.95))-tmp.mod.c$coefficients[1])/tmp.mod.c$coefficients[2]
+  cutoff975.c=(log(.975/(1-.975))-tmp.mod.c$coefficients[1])/tmp.mod.c$coefficients[2]
   cutoff99.c=(log(.99/(1-.99))-tmp.mod.c$coefficients[1])/tmp.mod.c$coefficients[2]
   
   cutoff85.r=(log(.85/(1-.85))-tmp.mod.r$coefficients[1])/tmp.mod.r$coefficients[2]
   cutoff90.r=(log(.90/(1-.90))-tmp.mod.r$coefficients[1])/tmp.mod.r$coefficients[2]
   cutoff95.r=(log(.95/(1-.95))-tmp.mod.r$coefficients[1])/tmp.mod.r$coefficients[2]
+  cutoff975.r=(log(.975/(1-.975))-tmp.mod.r$coefficients[1])/tmp.mod.r$coefficients[2]
   cutoff99.r=(log(.99/(1-.99))-tmp.mod.r$coefficients[1])/tmp.mod.r$coefficients[2]
   
   #output_2021$th.950[s]=round(predict(tmp.mod,newdata=data.frame(score=950),type='r'),3)
@@ -109,6 +111,7 @@ for(s in 1:length(species.list_2021)){
     abline(v=cutoff85.r,col='yellow',lwd=4)
     abline(v=cutoff90.r,col='orange',lwd=4)
     abline(v=cutoff95.r,col='red',lwd=4)
+    abline(v=cutoff975.r, col = 'green', lwd=4)
     abline(v=cutoff99.r,col='magenta',lwd=4)
   }
   if(confidence.plot=="yes"){
@@ -118,6 +121,7 @@ for(s in 1:length(species.list_2021)){
     abline(v=cutoff85.c,col='yellow',lwd=4)
     abline(v=cutoff90.c,col='orange',lwd=4)
     abline(v=cutoff95.c,col='red',lwd=4)
+    abline(v=cutoff975.c, col='green',lwd=4)
     abline(v=cutoff99.c,col='magenta',lwd=4)
   }
     
@@ -138,6 +142,7 @@ for(s in 1:length(species.list_2021)){
     
     output_2021$cutoff_90.r_conf=1/(1+exp(-output_2021$cutoff_90.r))
     output_2021$cutoff_95.r_conf=1/(1+exp(-output_2021$cutoff_95.r))
+    output_2021$cutoff_975.r_conf=1/(1+exp(-output_2021$cutoff_975.r))
     output_2021$cutoff_99.r_conf=1/(1+exp(-output_2021$cutoff_99.r))
   }
   
@@ -146,19 +151,21 @@ for(s in 1:length(species.list_2021)){
   ## We need to threshold at the values of interest
   dt$conf_pred_90 <- ifelse(dt$score >= cutoff90.c, 1, 0)
   dt$conf_pred_95 <- ifelse(dt$score >= cutoff95.c, 1, 0)
+  dt$conf_pred_975 <- ifelse(dt$score >= cutoff975.c, 1, 0)
   dt$conf_pred_99 <- ifelse(dt$score >= cutoff99.c, 1, 0)
   dt$r_pred_90 <- ifelse(dt$raw >= cutoff90.r, 1, 0)
   dt$r_pred_95 <- ifelse(dt$raw >= cutoff95.r, 1, 0)
+  dt$r_pred_975 <- ifelse(dt$raw >= cutoff975.r, 1, 0)
   dt$r_pred_99 <- ifelse(dt$raw >= cutoff99.r, 1, 0)
   
   ## Join the scores for precrec object
-  scores <- join_scores(dt$conf_pred_90, dt$conf_pred_95, dt$conf_pred_99)
-  labels <- join_scores(dt$correct, dt$correct, dt$correct)
-  dt.c <- mmdata(scores, labels, modnames = c("90", "95", "99"))
+  scores <- join_scores(dt$conf_pred_90, dt$conf_pred_95, dt$conf_pred_975, dt$conf_pred_99)
+  labels <- join_scores(dt$correct, dt$correct, dt$correct, dt$correct)
+  dt.c <- mmdata(scores, labels, modnames = c("90", "95", "975", "99"))
   
-  scores.r <- join_scores(dt$r_pred_90, dt$r_pred_95, dt$r_pred_99)
-  labels.r <- join_scores(dt$correct, dt$correct, dt$correct)
-  dt.r <- mmdata(scores.r, labels.r, modnames = c("90", "95", "99"))
+  scores.r <- join_scores(dt$r_pred_90, dt$r_pred_95, dt$r_pred_975, dt$r_pred_99)
+  labels.r <- join_scores(dt$correct, dt$correct, dt$correct, dt$correct)
+  dt.r <- mmdata(scores.r, labels.r, modnames = c("90", "95", "975", "99"))
   
   ## Calculate PRAUC curve (focuses on positive class)
   em.conf <- precrec::evalmod(dt.c)
@@ -176,13 +183,15 @@ for(s in 1:length(species.list_2021)){
   }
   
   ## PRAUC values
-  output_2021$PRAUC_90.c[s] <- pr_auc[1]
-  output_2021$PRAUC_95.c[s] <- pr_auc[2]
-  output_2021$PRAUC_99.c[s] <- pr_auc[3]
+  output_2021$PRAUC_90.c[s] <- pr_auc.conf[1]
+  output_2021$PRAUC_95.c[s] <- pr_auc.conf[2]
+  output_2021$PRAUC_975.c[s] <- pr_auc.conf[3]
+  output_2021$PRAUC_99.c[s] <- pr_auc.conf[4]
   
   output_2021$PRAUC_90.r[s] <- pr_auc.r[1]
   output_2021$PRAUC_95.r[s] <- pr_auc.r[2]
-  output_2021$PRAUC_99.r[s] <- pr_auc.r[3]
+  output_2021$PRAUC_975.r[s] <- pr_auc.r[3]
+  output_2021$PRAUC_99.r[s] <- pr_auc.r[4]
   
   ## Calculate a variety of prediction metrics
   pa_metrics.c <- dt |> 
@@ -190,7 +199,7 @@ for(s in 1:length(species.list_2021)){
     select(ID, correct, score) |> 
     PresenceAbsence::presence.absence.accuracy(na.rm = TRUE, 
                                                st.dev = FALSE, 
-                                               threshold = c(0.9, 0.95, 0.99),
+                                               threshold = c(0.9, 0.95, 0.975, 0.99),
                                                )
   
   pa_metrics.r <- dt |> 
@@ -198,15 +207,17 @@ for(s in 1:length(species.list_2021)){
     select(ID, correct, score) |> 
     PresenceAbsence::presence.absence.accuracy(na.rm = TRUE, 
                                                st.dev = FALSE, 
-                                               threshold = c(0.9, 0.95, 0.99),
+                                               threshold = c(0.9, 0.95, 0.975, 0.99),
     )
   
   ## Generate various traditional test metrics for each threshold from BirdNet Conf Score
   output_2021$PCC_90.c[s] <- pa_metrics.c$PCC[pa_metrics.c$threshold == 0.90]
   output_2021$PCC_95.c[s] <- pa_metrics.c$PCC[pa_metrics.c$threshold == 0.95]
+  output_2021$PCC_975.c[s] <- pa_metrics.c$PCC[pa_metrics.c$threshold == 0.975]
   output_2021$PCC_99.c[s] <- pa_metrics.c$PCC[pa_metrics.c$threshold == 0.99]
   output_2021$PCC_90.r[s] <- pa_metrics.r$PCC[pa_metrics.r$threshold == 0.90]
   output_2021$PCC_95.r[s] <- pa_metrics.r$PCC[pa_metrics.r$threshold == 0.95]
+  output_2021$PCC_975.r[s] <- pa_metrics.r$PCC[pa_metrics.r$threshold == 0.975]
   output_2021$PCC_99.r[s] <- pa_metrics.r$PCC[pa_metrics.r$threshold == 0.99]
   
   output_2021$Sensitivity_90.c[s] <- pa_metrics.c$sensitivity[pa_metrics.c$threshold == 0.90]
@@ -238,12 +249,17 @@ for(s in 1:length(species.list_2021)){
                      factor(dt$conf_pred_90, levels=c(0,1)))
   ConfMat95.c <- table(factor(dt$correct, levels=c(0,1)), 
                      factor(dt$conf_pred_95, levels=c(0,1)))
+  ConfMat975.c <- table(factor(dt$correct, levels=c(0,1)), 
+                       factor(dt$conf_pred_975, levels=c(0,1)))
   ConfMat99.c <- table(factor(dt$correct, levels=c(0,1)), 
                      factor(dt$conf_pred_99, levels=c(0,1)))
+  
   ConfMat90.r <- table(factor(dt$correct, levels=c(0,1)), 
                        factor(dt$r_pred_90, levels=c(0,1)))
   ConfMat95.r <- table(factor(dt$correct, levels=c(0,1)), 
                        factor(dt$r_pred_95, levels=c(0,1)))
+  ConfMat975.r <- table(factor(dt$correct, levels=c(0,1)), 
+                       factor(dt$r_pred_975, levels=c(0,1)))
   ConfMat99.r <- table(factor(dt$correct, levels=c(0,1)), 
                        factor(dt$r_pred_99, levels=c(0,1)))
   
@@ -257,6 +273,11 @@ for(s in 1:length(species.list_2021)){
     (ConfMat95.c[2,2] + ConfMat95.c[1,2]) == 0,
     NA,
     ConfMat95.c[2,2] / (ConfMat95.c[2,2] + ConfMat95.c[1,2])
+  )
+  output_2021$Precision_975.c[s] <- ifelse(
+    (ConfMat975.c[2,2] + ConfMat975.c[1,2]) == 0,
+    NA,
+    ConfMat975.c[2,2] / (ConfMat975.c[2,2] + ConfMat975.c[1,2])
   )
   output_2021$Precision_99.c[s] <- ifelse(
     (ConfMat99.c[2,2] + ConfMat99.c[1,2]) == 0,
@@ -273,6 +294,11 @@ for(s in 1:length(species.list_2021)){
     (ConfMat95.r[2,2] + ConfMat95.r[1,2]) == 0,
     NA,
     ConfMat95.r[2,2] / (ConfMat95.r[2,2] + ConfMat95.r[1,2])
+  )
+  output_2021$Precision_975.r[s] <- ifelse(
+    (ConfMat975.r[2,2] + ConfMat975.r[1,2]) == 0,
+    NA,
+    ConfMat975.r[2,2] / (ConfMat975.r[2,2] + ConfMat975.r[1,2])
   )
   output_2021$Precision_99.r[s] <- ifelse(
     (ConfMat99.r[2,2] + ConfMat99.r[1,2]) == 0,
@@ -291,6 +317,11 @@ for(s in 1:length(species.list_2021)){
     NA,
     ConfMat95.c[2,2] / (ConfMat95.c[2,2] + ConfMat95.c[2,1])
   )
+  output_2021$Recall_975.c[s] <- ifelse(
+    (ConfMat975.c[2,2] + ConfMat975.c[2,1]) == 0,
+    NA,
+    ConfMat975.c[2,2] / (ConfMat975.c[2,2] + ConfMat975.c[2,1])
+  )
   output_2021$Recall_99.c[s] <- ifelse(
     (ConfMat99.c[2,2] + ConfMat99.c[2,1]) == 0,
     NA,
@@ -306,6 +337,11 @@ for(s in 1:length(species.list_2021)){
     (ConfMat95.r[2,2] + ConfMat95.r[2,1]) == 0,
     NA,
     ConfMat95.r[2,2] / (ConfMat95.r[2,2] + ConfMat95.r[2,1])
+  )
+  output_2021$Recall_975.r[s] <- ifelse(
+    (ConfMat975.r[2,2] + ConfMat975.r[2,1]) == 0,
+    NA,
+    ConfMat975.r[2,2] / (ConfMat975.r[2,2] + ConfMat975.r[2,1])
   )
   output_2021$Recall_99.r[s] <- ifelse(
     (ConfMat99.r[2,2] + ConfMat99.r[2,1]) == 0,
@@ -371,12 +407,13 @@ output_2021_MaxPrec.r <- output_2021_long  |>
   ) |> 
   # Filter for recall >= 0.5
   filter(recall_value > 0) |> 
+  filter(threshold %in% c("95", "975", "99")) |> 
   # For each species, keep only the threshold with highest precision
   group_by(species) |> 
   filter(precision_value == max(precision_value)) |>
   filter(recall_value == max(recall_value)) |> 
   # Remove the temporary columns and maintain original format
-  #select(-recall_value, -precision_value) %>%
+  select(-recall_value, -precision_value) %>%
   # Sort if desired
   arrange(species, metric, threshold)
 
@@ -389,7 +426,8 @@ output_2021_MaxPrec <- output_2021_long  |>
     precision_value = value[metric == "Precision"]
   ) |> 
   # Filter for recall >= 0.5
-  filter(recall_value > 0)  |> 
+  filter(recall_value > 0)  |>
+  filter(threshold %in% c("95", "975", "99")) |> 
   # For each species, keep only the threshold with highest precision
   group_by(species) |> 
   filter(precision_value == max(precision_value)) |>  
@@ -401,7 +439,24 @@ output_2021_MaxPrec <- output_2021_long  |>
   bind_rows(output_2021_MaxPrec.r) |> 
   arrange(species) |> 
   select(-value, -metric) |> 
-  distinct(species, type, threshold, recall_value, precision_value)
+  distinct(species, type, threshold, recall_value, precision_value) |> 
+  select(-recall_value, -precision_value) |> 
+  group_by(species, type) |> 
+  filter(threshold == min(as.numeric(threshold))) |> 
+  mutate(type = ifelse(type == "c", "r_conf", "r")) |> 
+  pivot_wider(names_from = type, values_from = threshold, values_fill = NA) |> 
+  ungroup()
+
+## Add in the best threshold for Raw and Conf
+output_2021_best_thresh <- output_2021 |> 
+  left_join(output_2021_MaxPrec) |> 
+  select(species:cutoff_99.r_conf, BestThresh.r_conf = r_conf, BestThresh.r = r)
+
+## Take the 99th scores and see what we are missing
+out99 <- output_2021_long  |> 
+  # Create temporary dataframe with precision and recall paired
+  filter(threshold %in% c("95", "99") & metric %in% c("Precision", "Recall") & type == "r")
+
 
 par(mfrow=c(1,2))
 hist(output_2021$cutoff90.r_conf, xlab="Confidence Score", xlim=c(0,1),
@@ -411,7 +466,67 @@ hist(output_2021$cutoff90.r[-74], xlab="Logit Score", #xlim=c(0,1),
 
 write.csv(output_2021, here('./Data/Thresholds_2021_20230309_AllMetrics.csv'),row.names = F)
 write.csv(output_2021_MaxPrec, here('./Data/Thresholds_2021_20230309_BestPrecRec.csv'),row.names = F)
+write.csv(output_2021_best_thresh, here('./Data/Thresholds_2021_20230309_BestThreshold_95min.csv'),row.names = F)
 output_2021[output_2021$species=="Olive-sided Flycatcher",]
+
+output_2021_Sequential.r <- output_2021_long  |> 
+  # Create temporary dataframe with precision and recall paired
+  filter(type == "r") |> 
+  group_by(species, threshold) |> 
+  mutate(
+    recall_value = value[metric == "Recall"],
+    precision_value = value[metric == "Precision"]
+  ) |> 
+  # First try threshold 99
+  group_by(species) |> 
+  mutate(
+    selected_threshold = case_when(
+      any(threshold == "99" & recall_value >= 0.1) ~ "99",
+      any(threshold == "975" & recall_value >= 0.1) ~ "975",
+      any(threshold == "95" & recall_value >= 0.1) ~ "95",
+      any(threshold == "90" & recall_value >= 0.1) ~ "90",
+      TRUE ~ NA# default if none of the above meet criteria
+    )
+  ) |> 
+  filter(threshold == selected_threshold) |> 
+  # Rest of your original code
+  arrange(species, metric, threshold)
+
+output_2021_Sequential <- output_2021_long  |> 
+  # Create temporary dataframe with precision and recall paired
+  filter(type == "c") |> 
+  group_by(species, threshold) |> 
+  mutate(
+    recall_value = value[metric == "Recall"],
+    precision_value = value[metric == "Precision"]
+  ) |> 
+  # First try threshold 99
+  group_by(species) |> 
+  mutate(
+    selected_threshold = case_when(
+      any(threshold == "99" & recall_value >= 0.1) ~ "99",
+      any(threshold == "975" & recall_value >= 0.1) ~ "975",
+      any(threshold == "95" & recall_value >= 0.1) ~ "95",
+      any(threshold == "90" & recall_value >= 0.1) ~ "90",
+      TRUE ~ NA# default if none of the above meet criteria
+    )
+  ) |> 
+  filter(threshold == selected_threshold) |> 
+  # Rest of your original code
+  arrange(species, metric, threshold) |>
+  bind_rows(output_2021_Sequential.r) |> 
+  arrange(species) |> 
+  select(-value, -metric) |> 
+  distinct(species, type, threshold, recall_value, precision_value) |> 
+  select(-recall_value, -precision_value) |> 
+  group_by(species, type) |> 
+  filter(threshold == min(as.numeric(threshold))) |> 
+  mutate(type = ifelse(type == "c", "r_conf", "r")) |> 
+  pivot_wider(names_from = type, values_from = threshold, values_fill = NA) |> 
+  ungroup()
+
+
+
 
 #################################################################################################
 
