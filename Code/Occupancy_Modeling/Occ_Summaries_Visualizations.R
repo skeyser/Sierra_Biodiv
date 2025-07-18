76ef182646957436457bf6f675e0c7de57724199
Spencer Ryan Keyser <srk252@cornell.edu>
2025-07-18 10:54:38 -0500

    Modified for the new figures from occupancy models. Fig 2 in the current MS.

--- Code/Occupancy_Modeling/Occ_Summaries_Visualizations.R
+++ Code/Occupancy_Modeling/Occ_Summaries_Visualizations.R
@@ -47,8 +47,8 @@ logit2prob <- function(logit){
 ## -------------------------------------------------------------
 
 ## Load runs
-load("R:/Users/skeyser/Postdoc/MSOM_Ragged_JAGS_Summaries_95thresh.Rdata")
-load("R:/Users/skeyser/Postdoc/MSOM_Ragged_JAGS_Zout_95thresh.Rdata")
+#load("R:/Users/skeyser/Postdoc/MSOM_Ragged_JAGS_Summaries_95thresh.Rdata")
+#load("R:/Users/skeyser/Postdoc/MSOM_Ragged_JAGS_Zout_95thresh.Rdata")
 
 load(here("./Data/JAGS_Output/MSOM_Ragged_JAGS_Summaries_VarThresh_975min.Rdata"))
 load(here("./Data/JAGS_Output/MSOM_Ragged_JAGS_Zout_VarThresh_975min.Rdata"))
@@ -75,8 +75,8 @@ MCMCvis::MCMCsummary(out2, round = 2)
 
 
 
-MCMCvis::MCMCtrace(out2, params = c("mu.beta1"), n.eff = T)
-MCMCvis::MCMCtrace(out2, params = c("mu.beta2"), n.eff = T)
+MCMCvis::MCMCtrace(out2, params = c("mu.beta1"), n.eff = T, pdf = F)
+MCMCvis::MCMCtrace(out2, params = c("mu.beta2"), n.eff = T, pdf = F)
 
 ## Tree plot
 out2$parameters
@@ -85,7 +85,7 @@ out2$parameters
 ## Occupancy model
 com_eff <- MCMCvis::MCMCplot(out2,
          params = c(paste0("mu.beta", seq(1,9))),
-         ci = c(50,95),
+         ci = c(50,89),
          ref_ovl = TRUE,
          labels = c("Latitude", 
                     "Elevation", 
@@ -103,7 +103,7 @@ com_eff <- MCMCvis::MCMCplot(out2,
 ## Detection model
 MCMCvis::MCMCplot(out2,
                   params = c(paste0("mu.alpha", seq(1,3))),
-                  ci = c(50,95),
+                  ci = c(50,89),
                   ref_ovl = TRUE,
                   labels = c("Efforts Hours",
                              "JDate",
@@ -133,7 +133,10 @@ legend("topright", legend=c("95% CI", "Mean"),
 ## Histograms of species richness
 hist(out2$mean$Nsite, main = "Species Richness", breaks = 20)
 mean(out2$mean$Nsite)
+mean(out2$q2.5$Nsite)
+mean(out2$q97.5$Nsite)
 var(out2$mean$Nsite)
+sd(out2$mean$Nsite)
 
 
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -168,11 +171,11 @@ mcmc_draws <- mcmc_draws |>
     Parameter == "beta2" ~ "Elevation",
     Parameter == "beta3" ~ "Elevation^2",
     Parameter == "beta4" ~ "Precipitation",
-    Parameter == "beta5" ~ "Mean Fire Sev.: 1-5yr",
-    Parameter == "beta6" ~ "Mean Fire Sev.: 6-10yr",
-    Parameter == "beta7" ~ "Mean Fire Sev.: 11-35yr",
+    Parameter == "beta5" ~ "Fire Sev. 1-5yr",
+    Parameter == "beta6" ~ "Fire Sev. 6-10yr",
+    Parameter == "beta7" ~ "Fire Sev. 11-35yr",
     Parameter == "beta8" ~ "Canopy Cover",
-    Parameter == "beta9" ~ "Canopy Height Res."
+    Parameter == "beta9" ~ "Canopy Hght."
   )) |> 
   mutate(Pred_Cat = case_when(str_detect(Pred_Pretty, "Latitude|Elevation") ~ "Geographic",
                               Pred_Pretty == "Precipitation" ~ "Climate",
@@ -184,11 +187,11 @@ mcmc_draws <- mcmc_draws |>
                                 "Elevation",
                                 "Elevation^2",
                                 "Precipitation",
-                                "Mean Fire Sev.: 1-5yr",
-                                "Mean Fire Sev.: 6-10yr",
-                                "Mean Fire Sev.: 11-35yr",
+                                "Fire Sev. 1-5yr",
+                                "Fire Sev. 6-10yr",
+                                "Fire Sev. 11-35yr",
                                 "Canopy Cover",
-                                "Canopy Height Res." 
+                                "Canopy Hght." 
                               ))) |> 
   mutate(Pred_Cat = factor(Pred_Cat,
                            levels = rev(c(
@@ -205,19 +208,22 @@ pretty_eff <- ggplot(mcmc_draws, aes(y = Pred_Pretty, x = Draws,
   stat_halfeye(alpha = 0.7,
                .width = c(0.95, 0.89, 0.5)) +
   geom_vline(xintercept = 0, linetype = "dashed", 
-             color = "gray20", alpha = 0.4) +
+             color = "black", alpha = 0.6, size = 0.8) +
   scale_fill_manual(values = c(
     "Fire" = "#CC3311",           # red
     "Forest" = "#009988", # green
     "Climate" = "#0077BB",         # blue
-    "Geographic" = "#BBBBBB"        # purple
+    "Geographic" = "#7570b3"        # purple
   )) +
   scale_y_discrete(labels = function(x) {
     x <- gsub("\\^2", "²", x)  # Replace ^2 with ²
     return(x)
-  }) +
+  },
+  expand = c(0,0.1)) +
+  scale_x_continuous(limits = c(-0.7, 0.2),
+                     labels = scales::number_format(accuracy = 0.1, drop0trailing = T)) +
   theme_minimal() +
-  labs(x = "Standardized Effect Size",
+  labs(x = "Effect Size (Assemblage)",
        y = "") +
   theme(axis.text.y = element_text(hjust = 0, family = "sans", size = 12),
         axis.text.x = element_text(family = "sans", size = 12),
@@ -232,7 +238,11 @@ ggsave(plot = pretty_eff, filename = here("./Figures/Preliminary/CommunityEff_Ha
        height = 8, width = 8, dpi = 600)
 
 
-## Species-specific responses
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##
+## Subsection: Species-level effects plots
+##
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 sp.index <- read.csv(here("Code/Occupancy_Modeling/SpeciesIndex_Filtered_VarThresh.csv"))
 sp.index$Index <- as.character(sp.index$Index)
 
@@ -424,17 +434,17 @@ fire.eff.plot <- sp.resp |>
                                Par == "beta2" ~ "Elevation",
                                Par == "beta3" ~ "Elevation2",
                                Par == "beta4" ~ "Precipitation",
-                               Par == "beta5" ~ "Mean CBI: 1-5 yr post",
-                               Par == "beta6" ~ "Mean CBI: 6-10 yr post",
-                               Par == "beta7" ~ "Mean CBI: 11-35 yr post",
+                               Par == "beta5" ~ "Fire 1-5yr",
+                               Par == "beta6" ~ "Fire 6-10yr",
+                               Par == "beta7" ~ "Fire 11-35yr",
                                Par == "beta8" ~ "Canopy Cover",
                                Par == "beta9" ~ "Canopy Height Res.")) |>
-  filter(str_detect(ParPretty, "Mean CBI")) |> 
+  filter(str_detect(ParPretty, "Fire")) |> 
   mutate(ParPretty = factor(ParPretty,
                                levels = c(
-                               "Mean CBI: 1-5 yr post",
-                               "Mean CBI: 6-10 yr post",
-                               "Mean CBI: 11-35 yr post"))) |> 
+                               "Fire 1-5yr",
+                               "Fire 6-10yr",
+                               "Fire 11-35yr"))) |> 
   ggplot(aes(y = Mean, x = reorder(Species, -Mean))) +
   geom_hline(yintercept = 0, linetype = "dashed", color = "black", size = 1.2) +
   geom_pointrange(aes(ymin = LCI, ymax = UCI,
@@ -442,126 +452,80 @@ fire.eff.plot <- sp.resp |>
                         LCI > 0 ~ "Above",
                         UCI < 0 ~ "Below",
                         TRUE ~ "Intersects")), 
-                  size = 0.8,
+                  size = 0.5,
                   show.legend = F) +
   scale_color_manual(values = c("Above" = "#FF6600",
                                 "Below" = "#99CCFF",
                                 "Intersect" = "gray")) + 
+  scale_y_continuous(labels = scales::number_format(drop0trailing = T)) +
   coord_flip() + 
   theme_bw() +
+  theme(strip.background = element_blank(),
+        strip.text = element_text(family = "sans", size = 12),
+        axis.text.y = element_text(family = "sans", size = 9),
+        axis.text.x = element_text(family = "sans", size = 12),
+        axis.title = element_text(family = "sans", size = 12),
+        plot.title = element_text(hjust = 0.5)) + 
   xlab("Species") + 
-  ylab("Parameter Estimate") + 
+  ylab("Effect Size (Species)") + 
   facet_wrap(~ParPretty, nrow = 1)
 
 ggsave(plot = fire.eff.plot, filename = here("./Figures/Preliminary/MSOM_EffPlot_FireCovariates_SpVarThresh.jpg"),
-       height = 10, width = 10, dpi = 600)
+       height = 10, width = 12, dpi = 600)
 
 
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##
-## Subsection: Beta-diversity with MSOM
+## Subsection: Create Fig 2
 ##
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-aru_meta <- readr::read_csv(here("Data/ARU_120m.csv"))
-aru_meta$Cell_Unit <- paste0(aru_meta$cell_id, "_", aru_meta$unit_numbe)
+## Fire plus the overall effects
+fig2 <- pretty_eff + free(fire.eff.plot) + plot_layout(widths = c(0.5, 1)) + plot_annotation(tag_levels = 'A')
+ggsave(plot = fig2, filename = here("./Figures/Final/Fig2.jpg"),
+       height = 10, width = 10, dpi = 800)
 
-## Select the variables we are interested in for detection and occupancy
-aru_meta <- aru_meta |> select(Cell_Unit, 
-                               utme, utmn,
-                               X, Y,
-                               topo_elev, topo_tpi,
-                               tmx_bcm_mn, ppt_bcm_mn,
-                               fire1yr_cbi_mn, fire2_5yr_cbi_mn,
-                               fire6_10yr_cbi_mn, fire11_35yr_cbi_mn,
-                               standage_f3_mn, cpycovr_f3_mn) |> 
-  filter(Cell_Unit %in% cu.map$Cell_Unit) |> 
-  arrange(match(Cell_Unit, cu.map$Cell_Unit))
 
-load(here("./Data/JAGS_Data/MSOM_Ragged_2021_95cut.RData"))
-str(all3)
-## Z matrix
-nsite <- win.data.rag$nsite
-nspec <- win.data.rag$nspec
-nsamp <- dim(all3)[1]
-z <- array(NA, dim = c(nsite, nspec, nsamp))
-Jacc <- array(NA, dim = c(nsite, nspec, nsamp))
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##
+## Subsection: Stats on species responses to fire
+##
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-## For the 99 cut the range of Z is: 1205:(1204 + nspec*nsite)
-for(j in 1:nsamp){
-  cat(paste("\nMCMC sample", j, "\n"))
-  z[,,j] <- all3[j, 1447:(1446 + nspec*nsite)]
-}
+## Significant response to either 
+sp.resp |> 
+  filter(Par %in% c("beta5", "beta6")) |> 
+  mutate(Sig = ifelse(UCI < 0 | LCI > 0, "Sig", "NonSig")) |> 
+  group_by(Par) |> 
+  count(Sig)
 
-#saveRDS(z, file = here("./Data/JAGS_Data/Z_Array_Assembled_95thresh.RData"))
+sp.resp |> 
+  filter(Par %in% c("beta5", "beta6")) |> 
+  mutate(Sig = ifelse(UCI < 0 | LCI > 0, "Sig", "NonSig")) |> 
+  filter(Sig == "Sig") |>
+  distinct(Species) |> 
+  tally()
 
-# Restrict computations to observed species
-zobs <- z[,1:91,] # Species 1 to 145
-# Compute Jaccard index for sites and for species
-Jsite <- array(NA, dim = c(nsite, nsamp))
-Jspec <- array(NA, dim = c(91, nsamp))
-Jbetap <- array(NA, dim = c(nsite, nsamp))
-# Choose reference site and species for Jaccard indices
-ref.site <- 95 # Just choose first site
-ref.species <- 1 # Acorn Woodpecker (check object 'obs.occ')
-# Get posterior distributions for Jsite and Jspec (for references)
-for(k in 1:nsamp){
-  for(i in 1:nsite){ # Jaccard index for sites (in terms of shared species)
-    Jsite[i,k] <- sum(zobs[ref.site,,k] * zobs[i,,k]) / (sum(zobs[ref.site,,k]) +
-                                                           sum(zobs[i,,k]) - sum(zobs[ref.site,,k] * zobs[i,,k]))
-  }
-  for(i in 1:nspec){ # Jacc. index for species (in terms of shared sites)
-    Jspec[i,k] <- sum(zobs[,ref.species,k] * zobs[,i,k]) / (sum(zobs[,ref.species,k]) +
-                                                              sum(zobs[,i,k]) - sum(zobs[,ref.species,k] * zobs[,i,k]))
-  }
-}
 
-for(k in 1:nsamp){
-  test <- betapart::beta.pair(zobs[,,k], index.family = "sor")
-  sim <- test$beta.sim
-}
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##
+## Subsection: # of sites with atleast 50% high severity fire
+##
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+load(here("./Data/JAGS_Data/Occ2GDM_Data_SpThresh_975minMaxPrec.Rdata"))
 
-# NA's arise when a site has no species or a species no sites
-# Get posterior means, standard deviations and 95% CRI
-# Jaccard index for sites, compared to reference site 1
-pm <- apply(Jsite, 1, mean, na.rm = TRUE) # Post. mean of Jsite wrt. site 1
-psd <- apply(Jsite, 1, sd, na.rm = TRUE) # Post. sd of Jsite wrt. site 1
-cri <- apply(Jsite, 1, function(x) quantile(x, prob = c(0.025, 0.975), na.rm =
-                                              TRUE)) # CRI
-cbind('post. mean' = pm, 'post. sd' = psd, '2.5%' = cri[1,], '97.5%' = cri[2,])
+## Take the environmental data
+aru_meta <- OccData$SiteMeta
+aru_meta <- aru_meta |> 
+  select(-utme, -utmn) |> 
+  st_as_sf(coords = c("X", "Y"), crs = 4326) |> 
+  st_transform(crs = 3310) |> 
+  mutate(Lat = st_coordinates(geometry)[,2],
+         Long = st_coordinates(geometry)[,1]) |> 
+  st_drop_geometry()
 
-x <- 3 # size setting for plotting symbol
-
-JZ <- data.frame(Y = aru_meta$Y, X = aru_meta$X,
-                 Elevation = aru_meta$topo_elev,
-                 Beta = pm, Beta.sd = psd, Beta.cril = cri[1,], Beta.criu = cri[2,])
-
-JZsf <- JZ |> st_as_sf(coords = c("X", "Y"), crs = 4326)
-
-ggplot(JZsf) + 
-  geom_sf(aes(size = Beta)) + 
-  scale_size_continuous() + 
-  theme_bw()
-
-library(mapview)
-
-mapView(JZsf, zcol = "Beta")
-
-## Typical vegan based adonis2
-test <- zobs[,,1]
-test <- test[-rmsites,]
-aru_meta <- as.data.frame(aru_meta)
-aru_meta <- aru_meta[-rmsites,]
-colnames(test) <- paste0("Species", 1:91)
-vegmod <- vegan::adonis2(test ~ utmn + topo_elev + standage_f3_mn + cpycovr_f3_mn + fire6_10yr_cbi_mn + fire11_35yr_cbi_mn, data = aru_meta, method = "jaccard", by = "terms")
-vegmod
-
-# Jaccard index for species, compared with a reference species
-# (species 13, European Sparrowhawk)
-pm <- apply(Jspec, 1, mean, na.rm = TRUE) # Post. mean of Jspec wrt. species 1
-psd <- apply(Jspec, 1, sd, na.rm = TRUE) # Post. sd of Jspec wrt. species 1
-cri <- apply(Jspec, 1, function(x) quantile(x, prob = c(0.025, 0.975), na.rm =
-                                              TRUE)) # CRI
-tmp <- cbind('post. mean' = pm, 'post. sd' = psd, '2.5%' = cri[1,], '97.5%' = cri[2,])
-rownames(tmp) <- sp.names[[3]]
-print(tmp) # print in systematic order
-print(tmp[rev(order(tmp[,1])),]) # print in order of decreasing Jacc. values
+## Prop Fire
+aru_meta |>
+  select(Cell_Unit, fire1_5yr_high_prop, fire6_10yr_high_prop) |> 
+  filter(fire1_5yr_high_prop > 0.5 | fire6_10yr_high_prop > 0.5) |> 
+  distinct(Cell_Unit) |> 
+  tally()
